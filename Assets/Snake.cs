e33d26a7ae50d705c4fb1cb4ee6748bc46ee9f75
tsicroxE <arbanakus@gmail.com>
2019-03-09 17:09:57 -0800

    first commit for first game for first time playing with unity... FIRST!

---
+++ Assets/Snake.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+
+
+    Vector2 dir = Vector2.right;
+
+    // Keep Track of Tail
+    List<Transform> tail = new List<Transform>();
+
+    // Did the snake eat something?
+    bool ate = false;
+
+    // Tail Prefab
+    public GameObject tailPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Move", 0.3f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = Vector2.down;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = Vector2.left;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up; 
+    }
+
+    void Move()
+    {
+        // Save current position - Gap will be here
+        Vector2 v = transform.position;
+
+        //Move head into new direction now that there is a gap
+
+        // Ate something? Then insert new Element into gap
+        if (ate)
+        {
+            // Load Prefab into the world
+            GameObject g = (GameObject)Instantiate(tailPrefab,
+                                                  v,
+                                                  Quaternion.identity);
+
+            // Keep track of it in our tail list
+            tail.Insert(0, g.transform);
+
+            // Reset the flag
+            ate = false;
+        }
+
+        //Check for tail
+        if (tail.Count > 0)
+        {
+            // Move last tail Element to where the head was
+            tail.Last().position = v;
+
+            //Add to front of list, remove from the back
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+        transform.Translate(dir);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // Food?
+        if (coll.name.StartsWith("FoodPrefab"))
+        {
+            // Get longer in next Move call
+            ate = true;
+
+            // Remove the Food
+            Destroy(coll.gameObject);
+        }
+        // Collided with Tail or Border
+        else
+        {
+            // ToDo 'You lose' screen
+        }
+    }
+}
---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    void Spawn()
+    {
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+
+        Instantiate(foodPrefab, 
+                    new Vector2(x, y), 
+                    Quaternion.identity);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+}
